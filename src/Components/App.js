7301727e357b1d308bb70b085224106acdb68fe6
Asher Tuggle <ashertuggle@gmail.com>
2021-03-09 13:29:45 -0700

    added background image

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,7 @@
+import background from 'Images/talent-calc-bg.png'
+
 const App = () => (
-  <div className='app'>
+  <div className='app' style={{ backgroundImage: `url(${background})` }}>
     Hello There
   </div>
 )
